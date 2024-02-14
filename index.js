d40fbcbd5b9bdc0de3b3b10bf602dea0332ddcec
Aldara-Imperium <158221256+Aldara-Imperium@users.noreply.github.com>
2024-02-14 16:21:42 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/public'));
+app.use(function(req, res, next) {
+  res.status(404).sendFile(__dirname + '/404.html');
+  });
+  app.listen(3000, () => console.log('Server started'));
