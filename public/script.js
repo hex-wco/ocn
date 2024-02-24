b7b277415216d93d64d9d736b2c87e44502f6611
Aldara-Imperium <158221256+Aldara-Imperium@users.noreply.github.com>
2024-02-24 21:44:32 +0200

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,18 @@
+const menuIcon = document.getElementsByClassName("open-menu")[0];
+const navBar = document.getElementsByClassName("nav-bar")[0];
+
+menuIcon.addEventListener("click", function() {
+  navBar.classList.toggle("show-menu");
+});
+    document.getElementById("subscribe").addEventListener("click", function() {
+        window.location.href = "#"; 
+    });
+
+document.getElementById("subscribe").addEventListener("click", function() {
+        window.location.href = "#"; 
+    });
+
+document.getElementById("subscribe2").addEventListener("click", function() {
+        window.location.href = "#"; 
+    });
+
