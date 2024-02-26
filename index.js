e63d8a0a409a7878ca78151d0d9b67cd1012c92f
xdnw <jessepaleg@outlook.com>
2024-02-26 12:46:07 +0000

    use root folder

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/'));
 app.use(function(req, res, next) {
   res.status(404).sendFile(__dirname + '/404.html');
   });
